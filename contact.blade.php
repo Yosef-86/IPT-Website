408c5937d3009690c59fb6e61e12424fcb122fd2
mlnotml <mlnotmlatienza123@gmail.com>
2024-12-16 15:52:35 +0800

    Add files via upload

---
+++ contact.blade.php
@@ -0,0 +1,29 @@
+
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<x-layout>    
+<div class="bg-success w-100 h-100 d-flex row p-5 ">
+        <h1 class="text-center mt-5 text-white">Get in Touch</h1>
+        <div class="col text-center">
+            <h2 class="text-center mt-5 text-white">Contact us</h2>
+            <h2 class="text-center text-white">Group 2</h2>
+
+            <div class="row">
+                <div class="col text-white m-4">
+                    <h3>Email Address    :</h3>
+                    <h3>Phone Number  :</h3>
+                    <h3>Address   :</h3>
+                </div>
+                <div class="col m-4 text-white">
+                    <h3>eastwoods@gmail.com</h3>
+                    <h3>+0912-645-7154</h3>
+                    <h3>Banzon ibayo Balanga, City</h3>
+                </div>
+            </div>
+        </div>
+        <div class="col mt-5">
+        <iframe class="w-75 h-75"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1147.4598205596685!2d120.53647958338185!3d14.67980376558722!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x339641c0395eb9e9%3A0x7d3d02d68a830230!2sEASTWOODS%20Professional%20College%20of%20Science%20and%20Technology!5e0!3m2!1sen!2sph!4v1698200642533!5m2!1sen!2sph" style="border: 2px solid;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        
+        </div>
+    </div>
+</x-layout>
